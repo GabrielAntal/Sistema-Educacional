ad355f61e1963868da62e8d74effd421cc5ff35c
willTOP2 <54459687+willTOP2@users.noreply.github.com>
2019-11-03 13:07:34 -0300

    Add files via upload

---
+++ Sistema Educacional/src/Controller/AlunosModel.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Controller.AlunoDao;
+import java.sql.Date;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Willian
+ */
+public class AlunosModel {
+   
+ 
+    private String nome;
+    private String sobrenome;
+    private String rg;
+   private String Data;
+
+    public AlunosModel( String nome, String sobrenome, String rg, String Data) {
+      
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+         this.Data=Data;
+        
+    }
+
+ 
+
+    public AlunosModel() {
+     
+    }
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String Data) {
+        this.Data = Data;
+    }
+
+    
+
+ 
+
+  
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+   
+  public void salvar(AlunosModel aluno){
+    
+  }
+ 
+}
