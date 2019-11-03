f122c3b6e3195da03b10e5fbe5af68b0f68d6371
willTOP2 <54459687+willTOP2@users.noreply.github.com>
2019-11-03 13:10:52 -0300

    Add files via upload

--- Sistema Educacional/src/Controller/AlunoDao.java
+++ Sistema Educacional/src/Controller/AlunoDao.java
@@ -7,38 +7,80 @@ package Controller;
 
 import Model.AlunosModel;
 import Model.Conexao_bd;
+import Views.Alunos;
+import java.awt.List;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 /**
  *
  * @author Willian
  */
-public class AlunoDao extends AlunosModel {
+public class AlunoDao  {
     
     Conexao_bd c = new Conexao_bd();
+    PreparedStatement pst =null;
+    private AlunosModel aluno;
 
-
-    public void InserirAluno(AlunosModel aluno){
+    public  void InserirAluno(AlunosModel aluno){
         c.conecta();
-     
+    
         try {
-           
-            PreparedStatement pst = c.conexao.prepareStatement("insert into aluno(nome_aluno,sobrenome,rg_aluno) values(?,?,?)");
-            // pst.setString(1, "default");
-             pst.setString(1, aluno.getNome());
-               pst.setString(2, aluno.getSobrenome());
-                 pst.setString(3, aluno.getRg());
-                 pst.execute();
+              //SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+               PreparedStatement pst = c.conexao.prepareStatement("insert into aluno(nome_aluno,sobrenome,rg_aluno,data_nasc) values(?,?,?,?)");
+   
+          
+               
+              
+                pst.setString(1, aluno.getNome());
+                pst.setString(2, aluno.getSobrenome());
+                pst.setString(3, aluno.getRg());
+              pst.setString(4, aluno.getData());
+
+                pst.execute();
+
                 JOptionPane.showMessageDialog(null, "Cadastrado com sucesso");
-                c.desconecta();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "erro no Dao" +ex);
-        }
-    }
+                pst.close();
+
+           } catch (SQLException ex) {
+               JOptionPane.showMessageDialog(null, "erro no Dao" +ex);
+           }
+
+             c.desconecta();
+
+       }
+    
+   
+    public ArrayList<AlunosModel> listarAlunos(String al){
+            try {
+                c.conecta();
+                
+               ArrayList<AlunosModel> lista = new ArrayList<>();
+             PreparedStatement pst = c.conexao.prepareStatement("select nome_aluno,sobrenome, rg_aluno from aluno where rg_aluno like'%"+al+"%'");
+              pst.executeQuery();
+               c.rs.first();
+               
+               while(c.rs.next()){
+                   aluno.setNome(c.rs.getString("nome_aluno"));
+                     aluno.setSobrenome(c.rs.getString("sobrenome"));
+                    aluno.setRg(c.rs.getString("rg_aluno"));
+                    lista.add(aluno);
+               }
+             
+               return lista;
+             
+         } catch (Exception e) {
+         }
+         return null;
+
+       }
     
 }
