be39059c3079926536dbabd875dab785e327cf72
willTOP2 <willian.rodrygues145@gmail.com>
2019-11-12 13:42:32 -0300

    commite atualizado

--- Sistema Educacional/src/Controller/ControlAluno.java
+++ Sistema Educacional/src/Controller/ControlAluno.java
@@ -10,7 +10,10 @@ import Model.Conexao_bd;
 import Views.Alunos;
 
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 
 /**
@@ -18,33 +21,34 @@ import javax.swing.JOptionPane;
  * @author Willian
  */
 public class ControlAluno extends AlunoDao {
-    
+
     AlunosModel a = new AlunosModel();
     
-    public void AlunoControl(String nome, String sobrenome, String rg, String data){
 
-      AlunosModel a = new AlunosModel(nome, sobrenome, rg,data);
+        public void AlunoControl(String nome, String sobrenome, String rg, String data){
+
+          AlunosModel a = new AlunosModel(nome, sobrenome, rg,data);
 
 
-       if(nome.length()>0&&sobrenome.length()>0&&rg.length()>0&&data.length()>0){
-            
-           InserirAluno(a);
-       
-       }else{
-                JOptionPane.showMessageDialog(null, "insira os campos");
+           if(nome.length()>0&&sobrenome.length()>0&&rg.length()>0&&data.length()>0){
+               
+               InserirAluno(a);
+
+           }else{
+                    JOptionPane.showMessageDialog(null, "insira os campos", "aviso", JOptionPane.ERROR_MESSAGE);
+            }
+
+
         }
-           
-         
-    }
+        
     
-    
-    public void Alu(String n){
-        if(n!=null){
-            listarAlunos(n);
         }
-    }
+     
+      
+    
+  
     
     
     
               
-}
+
