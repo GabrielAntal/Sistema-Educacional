954adeeb632ade496a24c08ca579fcda1b2b60bd
Antal <44688111+GabrielAntal@users.noreply.github.com>
2019-11-27 22:12:41 -0300

    Add files via upload

--- Sistema Educacional/src/Controller/ProfissionalDao.java
+++ Sistema Educacional/src/Controller/ProfissionalDao.java
@@ -3,9 +3,7 @@ package Controller;
 
 import Model.Conexao_bd;
 import Model.ProfissionalModel;
-import Views.Professores;
-
-
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -29,69 +27,23 @@ public class ProfissionalDao extends Conexao_bd{
         conecta();
         
         try {
-            PreparedStatement pst = conexao.prepareStatement("INSERT INTO PROFISSIONAL(cod_prof,nome_prof, cpf, login, senha, perfil)"
+            PreparedStatement pst = conexao.prepareStatement("INSERT INTO PROFISSIONAL(cod_prof, nome_prof, cpf, senha, login, perfil)"
                     + " values(default,?,?,?,?,?); ");
-                   pst.setString(1, prof.getNome());
-                  pst.setString(2, prof.getCpf());
-                   pst.setString(3, prof.getLogin());
-                    pst.setString(4, prof.getSenha());
+                    pst.setString(1, prof.getNome());
+                    pst.setString(2, prof.getCpf());
+                     pst.setString(3, prof.getSenha());
+                    pst.setString(4, prof.getLogin());
                     pst.setString(5, prof.getPerfil());
                     pst.execute();
-                    JOptionPane.showMessageDialog(null, "Cadastrado!"); 
                     pst.close();
+                    JOptionPane.showMessageDialog(null, "Cadastrado!"); 
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Verificar"+e);
             
-        } 
-         
-    } 
+        }
         
-        public ArrayList<ProfissionalModel> mostrar_prof(String nomeProf){
-            
-            ArrayList<ProfissionalModel> list = new ArrayList<>();
-            conecta();
-         
-            try {
-                pst = conexao.prepareStatement("Select * from profissional where nome_prof ilike'"+ 
-                        nomeProf+"%'");
-                rs =pst.executeQuery();
-                ProfissionalModel profissional = new ProfissionalModel();
-                while(rs.next()){
-                     profissional.setCod(rs.getInt("cod_prof"));
-                    profissional.setNome(rs.getString("nome_prof"));
-                    profissional.setCpf(rs.getString("cpf"));
-                  profissional.setLogin(rs.getString("login"));
-                   profissional.setSenha(rs.getString("senha"));
-                    profissional.setPerfil(rs.getString("perfil"));
-                    
-                    list.add(profissional);
-                }
-                
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Verificar mostra_prof "+e);
-                
-            }
-            
-            return list;
-            
-        }
-        public void Excluir_Profissional(String prof){
-        conecta();
-            
-        try {
-            PreparedStatement pst = conexao.prepareStatement("DELETE FROM PROFISSIONAL WHERE cpf ='"+prof +"'");
-            pst.executeUpdate();
-            pst.close();
-             JOptionPane.showMessageDialog(null,"Profissional Excluído");
-        } catch (SQLException e) {
-             JOptionPane.showMessageDialog(null,"Verificar Excluir_Profissional    "+ e);
-        }
-        }
-    
-   
-  
-    
-    
+     }   
+        
     public void Atualizar_Profissional (ProfissionalModel prof){
             conecta();
             
@@ -119,15 +71,57 @@ public class ProfissionalDao extends Conexao_bd{
     
     
     
+    public void Excluir_Profissional(String prof){
+        conecta();
+            
+        try {
+            PreparedStatement pst = conexao.prepareStatement("DELETE FROM PROFISSIONAL WHERE cpf ='"+prof +"'");
+            pst.executeUpdate();
+            pst.close();
+             JOptionPane.showMessageDialog(null,"Profissional Excluído");
+        } catch (SQLException e) {
+             JOptionPane.showMessageDialog(null,"Verificar Excluir_Profissional    "+ e);
+        }
+    
+    
+    
+    
     }
          
      
         
+    public ArrayList<ProfissionalModel> mostrar_prof(String nomeProf){
+            
+            ArrayList<ProfissionalModel> list = new ArrayList<>();
+            conecta();
+         
+            try {
+                pst = conexao.prepareStatement("Select * from profissional where nome_prof ilike'%"+ 
+                        nomeProf+"%'");
+                rs =pst.executeQuery();
+                
+                while(rs.next()){
+                    ProfissionalModel profissional = new ProfissionalModel();
+                    profissional.setNome(rs.getString("nome_prof"));
+                    profissional.setCpf(rs.getString("cpf"));
+                    profissional.setSenha(rs.getString("senha"));
+                    profissional.setLogin(rs.getString("login"));
+                    profissional.setPerfil(rs.getString("perfil"));
+                    
+                    list.add(profissional);
+                }
+                
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Verificar mostra_prof "+e);
+                
+            }
+            
+            return list;
+            
+    }
+        
+        
         
         
        
-    
-    
-    
-    
-
+}
